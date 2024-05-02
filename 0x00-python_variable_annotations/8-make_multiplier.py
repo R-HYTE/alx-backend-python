af8eae4705658bb087864427cdadc273e1d5652e
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-03 00:02:16 +0300

    style: fix pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,12 +4,14 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Create a multiplier function that multiplies any float by a fixed multiplier.
-        
+    """
+    Create a multiplier function that multiplies float by a fixed multiplier.
+
     Args:
     multiplier (float): The multiplier to apply in the returned function.
-        
+
     Returns:
-    Callable[[float], float]: A function that takes a float and multiplies it by the multiplier.
+    Callable[[float], float]: A function that takes a float and
+                        multiplies it by the multiplier.
     """
     return lambda x: x * multiplier
