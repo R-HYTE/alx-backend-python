b2e8cc42013580bbc0fdbe29ed05ba317b6f4d0b
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-02 22:20:08 +0300

    style: Remove trailing whitespaces to adhere to pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,11 +4,11 @@
 
 def add(a: float, b: float) -> float:
     """Add two float numbers and return the result.
-    
+
     Args:
     a (float): The first number.
     b (float): The second number.
-    
+
     Returns:
     float: The sum of a and b.
     """
