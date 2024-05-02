4c9cad1c38e412a549a66804bb12bcf1ea7dd793
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-02 22:26:37 +0300

    feat: Concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that provides a string concatenation function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings and return the concatenated result.
+
+    Args:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Return:
+    str: The result of concatenating str1 and str2.
+    """
+    return str1 + str2
