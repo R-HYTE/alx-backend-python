555a56b96ed23ff0a8f93b679bbd48abb546571d
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-07 01:35:23 +0300

    refactor: simplify logic

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,12 @@ Includes the wait_n coroutine to demonstrate concurrent execution of coroutines
 """
 
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Execute the wait_random coroutine n times with the specified max_delay.
 
@@ -17,12 +18,9 @@ async def wait_n(n: int, max_delay: int) -> list:
         max_delay (int): The maximum delay passed to wait_random.
 
     Returns:
-        list: A list of float values representing the delays,
+        List[float]: A list of float values representing the delays,
             returned in the order they were completed.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
-    completed_delays = []
-    for completed_task in asyncio.as_completed(tasks):
-        result = await completed_task
-        completed_delays.append(result)
-    return completed_delays
+    completed_delays = await asyncio.gather(*tasks)
+    return sorted(completed_delays)
