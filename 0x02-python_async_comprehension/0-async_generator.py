b797461050dfff8781927a1ead2386695e5b1b3a
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-07 22:32:51 +0300

    refactor: modified logic to use the random() in random module

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,15 +11,15 @@ import random
 
 async def async_generator():
     """
-    This function is an asynchronous generator that yields random numbers
-    between 0 and 10, with a 1-second pause between each yield.
+    Asynchronously generates a sequence of random numbers
+    between 0 and 10, with a 1-second pause between each number.
+
+    Returns:
+        A float representing a random number between 0 and 10.
 
     Yields:
-        A random number between 0 and 10.
+        A float representing a random number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-if __name__ == "__main__":
-    asyncio.run(async_generator())
+        yield random.random() * 10
